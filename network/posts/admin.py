1760ae551c101f387ff0c16f4f693a2514357140
ivan_grigorev <rauscral@gmail.com>
2020-11-22 03:17:29 +0300

    first commit

---
+++ network/posts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author', 'id')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+
+
+admin.site.register(Post, PostAdmin)
